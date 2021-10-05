26e5c82f6610c69363ab8540843d04db3a456044
CFox04 <coltonfox2004@gmail.com>
2021-10-05 09:27:54 -0400

    Use Drawable instead of YDR.

--- ydrimport.py
+++ ydrimport.py
@@ -19,14 +19,14 @@ class ImportYdrXml(bpy.types.Operator, ImportHelper):
     def execute(self, context):
         
         #try:
-        ydr_xml = YDR.from_xml_file(self.filepath)
+        ydr_xml = Drawable.from_xml_file(self.filepath)
             #ybn_obj = composite_to_obj(ybn_xml.bounds, os.path.basename(self.filepath))
             #bpy.context.collection.objects.link(ybn_obj)
             #self.report({'INFO'}, 'YDR Successfully imported.')
         #except Exception as e:
             #self.report({'ERROR'}, f"YDR failed to import: {e}")
 
-        print(ydr_xml.drawable.lod_dist_high)
+        print(ydr_xml.lod_dist_high)
 
         return {'FINISHED'}
 
