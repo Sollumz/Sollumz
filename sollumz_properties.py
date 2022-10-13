db8de0b3e929e7cbea9730ecc30b5d2839ec3b49
Vicente <vicho0202@gmail.com>
2022-10-13 02:11:22 -0300

    Fixed vertex color picker

--- sollumz_properties.py
+++ sollumz_properties.py
@@ -705,6 +705,8 @@ def register():
         name="Vertex Color",
         subtype="COLOR",
         default=(1.0, 1.0, 1.0, 1.0),
+        min=0.0,
+        max=1.0,
         size=4
     )
     bpy.types.Scene.vert_paint_alpha = bpy.props.FloatProperty(
