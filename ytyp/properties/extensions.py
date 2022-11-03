e656a92b6d9e5981e532ccfd812e8e513b3c557f
Paul Azema <rigonkmalk@gmail.com>
2022-11-03 19:10:01 +0100

    feat(lightshaft): add intensity and flashiness properties

--- ytyp/properties/extensions.py
+++ ytyp/properties/extensions.py
@@ -111,6 +111,8 @@ class LightShaftExtensionProperties(bpy.types.PropertyGroup, BaseExtensionProper
     length: bpy.props.FloatProperty(name="Length")
     color: bpy.props.FloatVectorProperty(
         name="Color", subtype="COLOR", min=0, max=1, size=4, default=(1, 1, 1, 1))
+    intensity: bpy.props.IntProperty(name="Intensity")
+    flashiness: bpy.props.IntProperty(name="Flashiness")
     fade_in_time_start: bpy.props.FloatProperty(name="Fade In Time Start")
     fade_in_time_end: bpy.props.FloatProperty(name="Fade In Time End")
     fade_out_time_start: bpy.props.FloatProperty(name="Fade Out Time Start")
