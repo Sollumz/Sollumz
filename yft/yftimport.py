8ef81c0a40562d7244526fe1ed654eaecee31272
Colton Fox <46362482+CFox04@users.noreply.github.com>
2022-11-27 23:13:28 -0500

    Fix incorrect placement of vehicle windows when child is rotated

--- yft/yftimport.py
+++ yft/yftimport.py
@@ -153,7 +153,7 @@ def create_vehicle_windows(frag_xml: Fragment, materials: list[bpy.types.Materia
 
         try:
             mesh = create_vehicle_window_mesh(
-                window_xml, window_name, child_obj.location)
+                window_xml, window_name, child_obj.matrix_basis)
         except:
             import_op.report(
                 {"ERROR"}, f"Error during creation of vehicle window mesh:\n{format_exc()}")
@@ -249,9 +249,10 @@ def get_window_child(window_xml: WindowItem, child_objs: list[bpy.types.Object])
     return child_objs[child_id]
 
 
-def create_vehicle_window_mesh(window_xml: WindowItem, name: str, child_location: Vector):
+def create_vehicle_window_mesh(window_xml: WindowItem, name: str, child_matrix: Vector):
     verts = calculate_window_verts(window_xml)
-    verts = [vert - child_location for vert in verts]
+    verts = [(vert - child_matrix.translation) @ child_matrix
+             for vert in verts]
     faces = [[0, 1, 2, 3]]
 
     mesh = bpy.data.meshes.new(name)
