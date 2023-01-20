30b215963aad7dececb029988508321b5b026609
ooknumber13 <47336113+ooknumber13@users.noreply.github.com>
2023-01-20 10:45:39 -0500

    fixed incorrect default timecycle spelling (#535)

--- ytyp/properties/mlo.py
+++ ytyp/properties/mlo.py
@@ -27,7 +27,7 @@ class RoomProperties(bpy.types.PropertyGroup, MloArchetypeChild):
     bb_max: bpy.props.FloatVectorProperty(name="Bounds Max", subtype="XYZ")
     blend: bpy.props.FloatProperty(name="Blend", default=1)
     timecycle: bpy.props.StringProperty(
-        name="Timecycle", default="int_GasStation")
+        name="Timecycle", default="int_gasstation")
     secondary_timecycle: bpy.props.StringProperty(
         name="Secondary Timecycle")
     flags: bpy.props.PointerProperty(type=RoomFlags, name="Flags")
