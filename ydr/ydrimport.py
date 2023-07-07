Sollumz/Sollumz

commit 52f4850a974b3582f38571c6b414f20caaadd944

Check if drawable is armature before adding modifiers (#516)

ydr/ydrimport.py
@@ -594,9 +594,10 @@ def drawable_to_obj(drawable, filepath, name, bones_override=None, materials=Non
             model, materials, drawable.name, LODLevel.VERYLOW, bones, import_settings, armature)
         dobj.parent = obj
 
-    for model in obj.children:
-        if model.sollum_type != SollumType.DRAWABLE_MODEL:
-            continue
+    if armature is not None:
+        for model in obj.children:
+            if model.sollum_type != SollumType.DRAWABLE_MODEL:
+                continue
 
         for child in model.children:
             if child.sollum_type != SollumType.DRAWABLE_GEOMETRY:

commit 3f90424deaef9e316cdeaf8cd2df5383256c7d29

fix(ydrimport): add armature modifers to all models
The loop iterating the model children was indented outside the loop iterating the models, so it was only adding modifiers to the last model. Broken since 52f4850a974b3582f38571c6b414f20caaadd944.

ydr/ydrimport.py
@@ -613,12 +613,12 @@ def drawable_to_obj(drawable, filepath, name, bones_override=None, materials=Non
             if model.sollum_type != SollumType.DRAWABLE_MODEL:
                 continue
 
-        for child in model.children:
-            if child.sollum_type != SollumType.DRAWABLE_GEOMETRY:
-                continue
+            for child in model.children:
+                if child.sollum_type != SollumType.DRAWABLE_GEOMETRY:
+                    continue
 
-            mod = child.modifiers.new("Armature", "ARMATURE")
-            mod.object = obj
+                mod = child.modifiers.new("Armature", "ARMATURE")
+                mod.object = obj
 
     if len(drawable.lights) > 0:
         create_lights(drawable.lights, obj)