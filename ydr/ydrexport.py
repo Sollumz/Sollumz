ef26f0292b86c458257caf0d52e459570f77782a
Colton Fox <46362482+coltfox@users.noreply.github.com>
2023-07-10 10:49:38 -0400

    Add warning when embedded shape bounds have transforms

--- ydr/ydrexport.py
+++ ydr/ydrexport.py
@@ -764,6 +764,10 @@ def create_embedded_collision_xmls(drawable_obj: bpy.types.Object, drawable_xml:
             bound_xml = create_bound_xml(
                 child, auto_calc_inertia, auto_calc_volume)
 
+            if not bound_xml.composite_transform.is_identity:
+                logger.warning(
+                    f"Embedded bound '{child.name}' has transforms (location, rotation, scale) but is not parented to a Bound Composite. Parent the collision to a Bound Composite in order for the transforms to work in-game.")
+
         if bound_xml is not None:
             drawable_xml.bounds.append(bound_xml)
 
