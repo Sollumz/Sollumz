28bc145e5e0cff22ed8a08292b85bf7bbcdf5429
Colton Fox <46362482+coltfox@users.noreply.github.com>
2023-08-11 22:37:47 -0400

    Update to 0.4.0

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "author": "Skylumz and Colton",
     "description": "This plugins allows you to import/export codewalker xml files.",
     "blender": (3, 5, 1),
-    "version": (0, 3, 6),
+    "version": (0, 4, 0),
     "category": "Import-Export"
 }
 
