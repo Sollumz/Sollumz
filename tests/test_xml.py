548a8a43da4beb0e9611aaf0fe2a0df7c8003cac
alexguirre <alexguirre.aa@gmail.com>
2023-08-20 15:31:10 +0200

    Fix incorrect parsing of boolean XML values
    Fixes #714.

---
+++ tests/test_xml.py
@@ -0,0 +1,13 @@
+import pytest
+from Sollumz.cwxml.element import get_str_type
+
+@pytest.mark.parametrize("string, expected", (
+    ("true", True),
+    ("True", True),
+    ("TrUE", True),
+    ("false", False),
+    ("False", False),
+    ("FALsE", False),
+))
+def test_xml_bool(string, expected):
+    assert get_str_type(string) == expected
