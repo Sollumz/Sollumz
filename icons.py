fb335a749785436517bd29f992b1d343f88ddd60
alexguirre <alexguirre.aa@gmail.com>
2023-11-10 16:51:08 +0100

    fix: custom icons breaking after disabling and enabling addon
    The icons preview collection needed to be recreated on `register()`.
    Also done some small code refactoring.
    
    Fixes #803.

--- icons.py
+++ icons.py
@@ -6,47 +6,47 @@ from .sollumz_preferences import get_addon_preferences
 
 ICON_DIR = Path(__file__).parent / "icons"
 ICON_EXT = ".png"
-PREVIEW_COLLECTION_NAME = "main"
 
 
 class IconManager:
     def __init__(self, icon_dir: Path):
+        self._icon_dir = icon_dir
         self._icons = self._get_custom_icons(icon_dir)
-        self._preview_collections = {}
-        self._register_icons_from_directory(icon_dir)
+        self._icons_preview_collection = None
 
-    def _get_custom_icons(self, path: Path) -> set:
+    def _get_custom_icons(self, path: Path) -> set[str]:
         """Retrieve all .png files from a directory."""
         return {icon.stem for icon in path.glob(f"*{ICON_EXT}")}
 
-    def _register_icons_from_directory(self, path: Path):
-        pcoll = bpy.utils.previews.new()
-        for icon_name in self._icons:
-            icon_path = path / f"{icon_name}{ICON_EXT}"
-            pcoll.load(icon_name, str(icon_path), 'IMAGE')
-        self._preview_collections[PREVIEW_COLLECTION_NAME] = pcoll
+    def get_icon(self, name: str) -> int:
+        """Gets a specific icon ID by name."""
+        if name not in self._icons:
+            return 0
 
-    def load_icon(self, name: str) -> int:
-        """Load a specific icon by name."""
-        if name in self._icons:
-            pcoll = self._preview_collections[PREVIEW_COLLECTION_NAME]
-            return pcoll[name].icon_id
-        return 0
+        return self._icons_preview_collection[name].icon_id
 
     def icon_label(self, name: str, panel: bpy.types.Panel):
         """Render an icon label as text layout."""
-        if name in self._icons:
-            if name != "sollumz_icon":
-                return panel.layout.label(text="", icon_value=self.load_icon(name))
+        if name not in self._icons:
+            return
+
+        if name != "sollumz_icon":
+            panel.layout.label(text="", icon_value=self.get_icon(name))
+        else:
             preferences = get_addon_preferences(bpy.context)
             if preferences.sollumz_icon_header:
-                return panel.layout.label(text="", icon_value=self.load_icon(name))
-        return
+                panel.layout.label(text="", icon_value=self.get_icon(name))
+
+    def register(self):
+        pcoll = bpy.utils.previews.new()
+        for icon_name in self._icons:
+            icon_path = self._icon_dir / f"{icon_name}{ICON_EXT}"
+            pcoll.load(icon_name, str(icon_path), "IMAGE")
+        self._icons_preview_collection = pcoll
 
     def unregister(self):
-        for pcoll in self._preview_collections.values():
-            bpy.utils.previews.remove(pcoll)
-        self._preview_collections.clear()
+        bpy.utils.previews.remove(self._icons_preview_collection)
+        self._icons_preview_collection = None
 
 
 # Global icon manager instance
@@ -54,8 +54,9 @@ icon_manager = IconManager(ICON_DIR)
 
 ICON_GEOM_TOOL = str(ICON_DIR / "sollumz.tool")
 
+
 def register():
-    pass  # Icons are already loaded during the instantiation of icon_manager
+    icon_manager.register()
 
 
 def unregister():
