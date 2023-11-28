e9359174423c57d3b77838a8243aef21475b5797
alexguirre <alexguirre.aa@gmail.com>
2023-11-28 16:43:48 +0100

    tweak: link wiki and issue tracker in addon info

--- __init__.py
+++ __init__.py
@@ -19,10 +19,12 @@ from . import sollumz_tool
 bl_info = {
     "name": "Sollumz",
     "author": "Skylumz and Colton",
-    "description": "This plugins allows you to import/export codewalker xml files.",
+    "description": "This plugins allows you to import/export CodeWalker XML files.",
     "blender": (4, 0, 0),
     "version": (2, 3, 1),
-    "category": "Import-Export"
+    "category": "Import-Export",
+    "doc_url": "https://sollumz.gitbook.io/sollumz-wiki/",
+    "tracker_url": "https://github.com/Skylumz/Sollumz/issues",
 }
 
 
