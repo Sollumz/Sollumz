Sollumz/Sollumz

commit bac5e485aabc08a40adc5725dd76bcb2e732e1b2

fix(ydr): export correct light position when `matrix_parent_inverse` is set
When `matrix_parent_inverse` is set, `location` is not what we want
(i.e. coordinates relative to parent). Also, using `matrix_world`
instead of `matrix_local` because lights object may not be a direct
child of the parent object (e.g. on import we create a wrapper object
for light, which could end up transformed in some way by the user).

Fixes #865.

ydr/lights.py
@@ -148,15 +148,15 @@ def create_xml_lights(parent_obj: bpy.types.Object, armature_obj: Optional[bpy.t
 
     for child in parent_obj.children_recursive:
         if child.type == "LIGHT" and child.data.sollum_type != LightType.NONE:
-            light_xmls.append(create_light_xml(child, armature_obj))
+            light_xmls.append(create_light_xml(child, parent_obj, armature_obj))
 
     return light_xmls
 
 
-def create_light_xml(light_obj: bpy.types.Object, armature_obj: Optional[bpy.types.Object] = None):
+def create_light_xml(light_obj: bpy.types.Object, parent_obj: bpy.types.Object, armature_obj: Optional[bpy.types.Object] = None):
     light_xml = Light()
-    light_xml.position = light_obj.location
-    mat = light_obj.matrix_basis
+    mat = parent_obj.matrix_world.inverted() @ light_obj.matrix_world
+    light_xml.position = mat.to_translation()
     set_light_xml_direction(light_xml, mat)
     set_light_xml_tangent(light_xml, mat)
 

commit 9a902c8649f0d2bd006b8562c2966ca301978c44

fix(ydr): export correct light position when attached to bones
When attached to a bone, the light position is relative to the bone.

The previous commit bac5e485aabc08a40adc5725dd76bcb2e732e1b2 broke it.

ydr/lights.py
@@ -155,13 +155,17 @@ def create_xml_lights(parent_obj: bpy.types.Object, armature_obj: Optional[bpy.t
 
 def create_light_xml(light_obj: bpy.types.Object, parent_obj: bpy.types.Object, armature_obj: Optional[bpy.types.Object] = None):
     light_xml = Light()
-    mat = parent_obj.matrix_world.inverted() @ light_obj.matrix_world
+
+    root_mat = parent_obj.matrix_world
+    if armature_obj is not None:
+        bone = set_light_xml_bone_id(light_xml, armature_obj.data, light_obj)
+        if bone is not None:
+            root_mat = root_mat @ bone.matrix_local
+
+    mat = root_mat.inverted() @ light_obj.matrix_world
     light_xml.position = mat.to_translation()
     set_light_xml_direction(light_xml, mat)
     set_light_xml_tangent(light_xml, mat)
-
-    if armature_obj is not None:
-        set_light_xml_bone_id(light_xml, armature_obj.data, light_obj)
 
     set_light_xml_properties(light_xml, light_obj.data)
 
@@ -183,8 +187,12 @@ def set_light_xml_bone_id(light_xml: Light, armature: bpy.types.Armature, light_
         if not isinstance(constraint, bpy.types.CopyTransformsConstraint):
             continue
 
-        bone = constraint.subtarget
-        light_xml.bone_id = armature.bones[bone].bone_properties.tag
+        bone_name = constraint.subtarget
+        bone = armature.bones[bone_name]
+        light_xml.bone_id = bone.bone_properties.tag
+        return bone
+
+    return None
 
 
 def set_light_xml_properties(light_xml: Light, light_data: bpy.types.Light):