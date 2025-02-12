1d7c0e0674544058f5ed77e8a88d9adc03a30453
alexguirre <alexguirre.aa@gmail.com>
2025-02-12 23:54:39 +0100

    wip: versioning for multiselect collections

--- versioning/versioning_260.py
+++ versioning/versioning_260.py
@@ -28,7 +28,28 @@ def update_archetype_uuids_in_scene(scene: Scene):
                 t["mlo_archetype_uuid"] = arch_uuid
 
 
+def update_archetype_multiselect_collections_in_scene(scene: Scene):
+    """With multiselection the actual CollectionProperty is at 'propname_' while 'propname' is a regular Python
+    property that returns a wrapper for more ergonomic use of multiselection collections.
+
+    Rename the old CollectionProperties.
+    """
+    from .versioning_230 import move_renamed_prop
+    for ytyp in scene.ytyps:
+        move_renamed_prop(ytyp, "archetypes", "archetypes_")
+        for arch in ytyp.get("archetypes_", []):
+            move_renamed_prop(arch, "rooms", "rooms_")
+            move_renamed_prop(arch, "portals", "portals_")
+            move_renamed_prop(arch, "entities", "entities_")
+            move_renamed_prop(arch, "timecycle_modifiers", "timecycle_modifiers_")
+            move_renamed_prop(arch, "entity_sets", "entity_sets_")
+
+
 def do_versions(data_version: int, data: BlendData):
     if data_version < 7:
         for scene in data.scenes:
             update_archetype_uuids_in_scene(scene)
+
+    if data_version < 8:
+        for scene in data.scenes:
+            update_archetype_multiselect_collections_in_scene(scene)
