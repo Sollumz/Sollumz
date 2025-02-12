79dd3a1a8113c2c7711d5a411e2cf06d897c3bbc
alexguirre <alexguirre.aa@gmail.com>
2025-02-12 23:19:42 +0100

    wip: fix versioning after multiselection changes

--- tests/versioning/test_versioning_260.py
+++ tests/versioning/test_versioning_260.py
@@ -10,23 +10,24 @@ def test_versioning_archetype_uuids():
     def _check_uuid(uuid: str, who):
         assert len(uuid) == UUID_LENGTH, f"UUID of '{who}' is invalid: {uuid}"
         assert uuid not in all_uuids, f"UUID of '{who}' already in use: {uuid}"
-        all_uuids.add(arch.uuid)
+        all_uuids.add(uuid)
 
     for scene in data.scenes:
         for ytyp in scene.ytyps:
-            for arch in ytyp.archetypes:
-                _check_uuid(arch.uuid, arch)
-                for e in arch.entities:
-                    _check_uuid(e.uuid, e)
-                    assert e.mlo_archetype_uuid == arch.uuid
-                for s in arch.entity_sets:
-                    _check_uuid(s.uuid, s)
-                    assert s.mlo_archetype_uuid == arch.uuid
-                for p in arch.portals:
-                    _check_uuid(p.uuid, p)
-                    assert p.mlo_archetype_uuid == arch.uuid
-                for r in arch.rooms:
-                    _check_uuid(r.uuid, r)
-                    assert r.mlo_archetype_uuid == arch.uuid
-                for tcm in arch.timecycle_modifiers:
-                    assert tcm.mlo_archetype_uuid == arch.uuid
+            for arch in ytyp.get("archetypes", []):
+                arch_uuid = arch["uuid"]
+                _check_uuid(arch_uuid, arch)
+                for e in arch.get("entities", []):
+                    _check_uuid(e["uuid"], e)
+                    assert e["mlo_archetype_uuid"] == arch_uuid
+                for s in arch.get("entity_sets", []):
+                    _check_uuid(s["uuid"], s)
+                    assert s["mlo_archetype_uuid"] == arch_uuid
+                for p in arch.get("portals", []):
+                    _check_uuid(p["uuid"], p)
+                    assert p["mlo_archetype_uuid"] == arch_uuid
+                for r in arch.get("rooms", []):
+                    _check_uuid(r["uuid"], r)
+                    assert r["mlo_archetype_uuid"] == arch_uuid
+                for tcm in arch.get("timecycle_modifiers", []):
+                    assert tcm["mlo_archetype_uuid"] == arch_uuid
--- versioning/versioning_260.py
+++ versioning/versioning_260.py
@@ -9,22 +9,23 @@ from bpy.types import (
 def update_archetype_uuids_in_scene(scene: Scene):
     from uuid import uuid4
     for ytyp in scene.ytyps:
-        for arch in ytyp.archetypes:
-            arch.uuid = str(uuid4())
-            for e in arch.entities:
-                e.uuid = str(uuid4())
-                e.mlo_archetype_uuid = arch.uuid
-            for s in arch.entity_sets:
-                s.uuid = str(uuid4())
-                s.mlo_archetype_uuid = arch.uuid
-            for p in arch.portals:
-                p.uuid = str(uuid4())
-                p.mlo_archetype_uuid = arch.uuid
-            for r in arch.rooms:
-                r.uuid = str(uuid4())
-                r.mlo_archetype_uuid = arch.uuid
-            for t in arch.timecycle_modifiers:
-                t.mlo_archetype_uuid = arch.uuid
+        for arch in ytyp.get("archetypes", []):
+            arch_uuid = str(uuid4())
+            arch["uuid"] = arch_uuid
+            for e in arch.get("entities", []):
+                e["uuid"] = str(uuid4())
+                e["mlo_archetype_uuid"] = arch_uuid
+            for s in arch.get("entity_sets", []):
+                s["uuid"] = str(uuid4())
+                s["mlo_archetype_uuid"] = arch_uuid
+            for p in arch.get("portals", []):
+                p["uuid"] = str(uuid4())
+                p["mlo_archetype_uuid"] = arch_uuid
+            for r in arch.get("rooms", []):
+                r["uuid"] = str(uuid4())
+                r["mlo_archetype_uuid"] = arch_uuid
+            for t in arch.get("timecycle_modifiers", []):
+                t["mlo_archetype_uuid"] = arch_uuid
 
 
 def do_versions(data_version: int, data: BlendData):
