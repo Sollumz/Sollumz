0c4164fc4bf4c99e3cdc26b1b3e8bf56ce2c6ece
alexguirre <alexguirre.aa@gmail.com>
2025-11-22 18:38:20 +0100

    fix(versioning): Blender 5.0 compatibility in versioning 2.5.0
    Somehow this one still worked in 5.0 and tests passed, but supposedly it
    shouldn't have as we weren't using `bl_system_properties_get`. Either
    way, change it to be consistent with the other versioning code.

--- versioning/versioning_250.py
+++ versioning/versioning_250.py
@@ -1,13 +1,19 @@
 """Handle changes between 2.5.0 and 2.6.0."""
 
+import bpy
 from bpy.types import (
     BlendData,
     Material,
 )
 
+from .versioning_230 import get_src_props
 
 def update_mat_paint_layer(mat: Material):
-    old_paint_layer_int = mat.get("sollumz_paint_layer", None)
+    src_props = get_src_props(mat)
+    if src_props is None:
+        return
+
+    old_paint_layer_int = src_props.get("sollumz_paint_layer", None)
     if old_paint_layer_int is None:
         return
 
@@ -26,7 +32,8 @@ def update_mat_paint_layer(mat: Material):
         from ..yft.properties import _set_mat_paint_layer
         _set_mat_paint_layer(mat, new_paint_layer_int)
 
-    del mat["sollumz_paint_layer"]
+    if bpy.app.version < (5, 0, 0):
+        del mat["sollumz_paint_layer"]
 
 
 def do_versions(data_version: int, data: BlendData):
