c366f9f2a974bda5e6f46da7f0ee9eabf91b9cf7
alexguirre <alexguirre.aa@gmail.com>
2026-01-13 17:00:32 +0100

    chore(deps): bump szio to 1.1.0.dev0
    Main changes:
    - Fixed #1139 (Sollumz/szio@cfd8f54c6ee9d3c35a4d9d261e097efad68d0a9d)
    - Fixed incorrect `requiredImap` XML tag (Sollumz/szio@4f7303a36e098a08ac9028c76d9630102615194c)

--- dependencies.py
+++ dependencies.py
@@ -40,8 +40,8 @@ DEPENDENCIES = (
         "Core functionality for import/export of asset files.",
         True,
         "",
-        "1.0.0",
-        ("2e9218506eeefa580e4b4c7fb87b076d6b1414f07608f2725c7a0616978b563a",),
+        "1.1.0.dev0",
+        ("fad3897197c45a15f85d1a76e20eb2f7bf2eb4ea825e01d56aab63dbf4df109d",),
     ),
     Dependency(
         "pymateria",
