Sollumz/Sollumz

commit 59a978518f0a0cfef7443c99642f4021e9c2b71b

feat: support export with packed textures
Adds support for exporting embedded textures from images packed in the
.blend as suggested in #1128.

deps: bump szio to 1.1.0.dev1
Main changes:
 - Embedded textures now use the DataSource class.
 - Embedded textures are not automatically saved to a folder by szio
   anymore (it is done on Sollumz side now).

iecontext.py
@@ -1,10 +1,13 @@
 import contextlib
 import os
 import shutil
+from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
-from collections.abc import Sequence
+
 from szio.gta5 import Asset, AssetFormat, AssetTarget, save_asset
+from szio.types import DataSource
+
 from .ydr.vertex_buffer_builder_domain import VBBuilderDomain
 
 
@@ -44,8 +47,8 @@ class ExportBundle:
     secondary_assets: tuple[tuple[str, Asset], ...]
     """Additional assets exported along with the main asset, e.g. _hi.yft or .yld. Tuple of tuples like (suffix, asset)."""
 
-    files_to_copy: tuple[str | os.PathLike]
-    """Files to copy to a folder with same name as the asset, generally embedded textures."""
+    extra_files: tuple[DataSource, ...]
+    """Additional files to write to a folder with same name as the asset, generally embedded textures."""
 
     def save(self, directory: Path):
         """Writes the whole bundle to disk at the specified directory."""
@@ -61,17 +64,17 @@ class ExportBundle:
         for suffix, asset in self.secondary_assets:
             save_asset(asset, directory, self.asset_name + suffix, tool_metadata, gen8_directory, gen9_directory)
 
-        do_copy_files = self.files_to_copy and (
-            # We only use files_to_copy for embedded textures, which are only really needed for CWXML. Initially, these
+        do_write_extra_files = self.extra_files and (
+            # We only use extra_files for embedded textures, which are only really needed for CWXML. Initially, these
             # were always copied but users requested that this not be done for native format.
-            # If we start using files_to_copy for something else, we will need to rework this.
+            # If we start using extra_files for something else, we will need to rework this.
             main_asset.ASSET_FORMAT == AssetFormat.CWXML
             or (
                 main_asset.ASSET_FORMAT == AssetFormat.MULTI_TARGET and AssetFormat.CWXML in main_asset.target_formats()
             )
         )
 
-        if do_copy_files:
+        if do_write_extra_files:
             if main_asset.ASSET_FORMAT == AssetFormat.MULTI_TARGET and len(main_asset.target_versions()) > 1:
                 output_dirs = (gen8_directory, gen9_directory)
             else:
@@ -79,13 +82,16 @@ class ExportBundle:
 
             for d in output_dirs:
                 res_directory = d / self.asset_name
-                for file in self.files_to_copy:
-                    if os.path.isfile(file):
-                        res_directory.mkdir(exist_ok=True)
-                        dst_file = res_directory / os.path.basename(file)
-                        # check if paths are the same because if they are, no need to copy (and would throw an error otherwise)
-                        if not dst_file.exists() or not dst_file.samefile(file):
-                            shutil.copy(file, dst_file)
+                for src_data in self.extra_files:
+                    res_directory.mkdir(exist_ok=True)
+                    dst_file = res_directory / src_data.name
+
+                    if (src_file := getattr(src_data, "filepath", None)) and dst_file.samefile(src_file):
+                        # If src_data is a file and paths are the same, no need to copy (and would break otherwise)
+                        continue
+
+                    with src_data.open() as src, dst_file.open("wb") as dst:
+                        shutil.copyfileobj(src, dst)
 
     def is_valid(self) -> bool:
         """Checks whether the export operation was successful."""
@@ -116,7 +122,7 @@ class ExportContext:
         main_asset: Asset | None,
         /,
         *secondary_assets: tuple[str, Asset | None],
-        files_to_copy: Sequence[str | os.PathLike] = (),
+        extra_files: Sequence[DataSource | None] = (),
     ) -> ExportBundle:
         """Creates an `ExportBundle` from the given assets and optional files.
 
@@ -124,14 +130,14 @@ class ExportContext:
             main_asset: The primary asset produced during export. Can be None if export failed.
             secondary_assets: Optional secondary assets represented as (suffix, asset) pairs. Only non-None assets will
                 be included in the bundle.
-            files_to_copy: Additional files to copy into a subdirectory named after the asset, typically used for
+            extra_files: Additional files to write into a subdirectory named after the asset, typically used for
                 embedded resources like textures.
         """
         return ExportBundle(
             self.asset_name,
             main_asset,
             tuple(s for s in secondary_assets if s[1] is not None),
-            tuple(files_to_copy),
+            tuple(f for f in extra_files if f is not None),
         )
 
 

tests/test_import_export.py
@@ -1,9 +1,17 @@
+import bpy
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 from pathlib import Path
 from xml.etree import ElementTree as ET
-from .shared import is_tmp_dir_available, tmp_path as tmp_path_with_subdir, glob_assets, asset_path
+from .shared import (
+    is_tmp_dir_available,
+    tmp_path as tmp_path_with_subdir,
+    glob_assets,
+    asset_path,
+    load_blend_data,
+    requires_szio_native,
+)
 from ..ydr.ydrimport import import_ydr
 from ..ydr.ydrexport import export_ydr
 from ..yft.yftimport import import_yft
@@ -205,3 +213,116 @@ def test_import_dedupe_hi_yft_files(files: str, expected_deduped_files: str):
     from ..sollumz_operators import SOLLUMZ_OT_import_assets
     res = SOLLUMZ_OT_import_assets._dedupe_hi_yft_filenames(None, files)
     assert_equal(res, expected_deduped_files)
+
+
+DEFAULT_EXPORT_SETTINGS = {
+    "target_formats": {"NATIVE", "CWXML"},
+    "target_versions": {"GEN8", "GEN9"},
+    "limit_to_selected": True,
+    "exclude_skeleton": False,
+    "ymap_exclude_entities": False,
+    "ymap_box_occluders": False,
+    "ymap_model_occluders": False,
+    "ymap_car_generators": False,
+    "apply_transforms": False,
+    "mesh_domain": "FACE_CORNER"
+}
+
+
+DEFAULT_IMPORT_SETTINGS = {
+    "import_as_asset": False,
+    "split_by_group": True,
+    "import_ext_skeleton": False,
+    "frag_import_vehicle_windows": False,
+    "ymap_skip_missing_entities": True,
+    "ymap_exclude_entities": False,
+    "ymap_box_occluders": False,
+    "ymap_model_occluders": False,
+    "ymap_car_generators": False,
+    "ymap_instance_entities": False,
+    "ytyp_mlo_instance_entities": True,
+}
+
+
+def test_export_model_with_packed_textures(tmp_path: Path):
+    data = load_blend_data("model_with_packed_textures.blend")
+
+    # .blend was saved with the object to export already selected
+    bpy.ops.sollumz.export_assets(
+        directory=str(tmp_path.absolute()),
+        direct_export=True,
+        use_custom_settings=True,
+        **DEFAULT_EXPORT_SETTINGS,
+    )
+
+    expected_contents = data.images["test_image.dds"].packed_file.data
+    for expected_file in [
+        tmp_path / "gen8" / "test_model" / "test_image.dds",
+        tmp_path / "gen9" / "test_model" / "test_image.dds",
+    ]:
+        assert expected_file.is_file()
+        assert expected_file.read_bytes() == expected_contents
+
+
+@requires_szio_native
+@pytest.mark.parametrize("version_dir", ("gen8", "gen9"))
+def test_import_model_with_embedded_textures_extract_to_import_dir(tmp_path: Path, version_dir: str):
+    bpy.ops.wm.read_homefile()
+
+    ydr_filename = "model_with_embedded_textures.ydr"
+    ydr_path = asset_path(version_dir, ydr_filename)
+
+    # Copy to temp dir
+    (tmp_path / ydr_filename).write_bytes(ydr_path.read_bytes())
+
+    expected_file = tmp_path / "model_with_embedded_textures" / "test_image.dds"
+    assert not expected_file.is_file()
+
+    bpy.ops.sollumz.import_assets(
+        directory=str(tmp_path.absolute()),
+        files=[{"name": ydr_path.name}],
+        use_custom_settings=True,
+        **DEFAULT_IMPORT_SETTINGS,
+    )
+
+    assert expected_file.is_file()
+    assert expected_file.read_bytes().startswith(b"DDS ")
+
+
+@requires_szio_native
+def test_export_to_same_dir_as_import_and_textures_are_exported_correctly(tmp_path: Path):
+    # Check that there are no errors when exporting embedded textures and their source and
+    # destination file paths are same.
+
+    bpy.ops.wm.read_homefile()
+
+    ydr_filename = "model_with_embedded_textures.ydr"
+    ydr_path = asset_path("gen8", ydr_filename)
+
+    # Copy to temp dir
+    (tmp_path / ydr_filename).write_bytes(ydr_path.read_bytes())
+
+    bpy.ops.sollumz.import_assets(
+        directory=str(tmp_path.absolute()),
+        files=[{"name": ydr_path.name}],
+        use_custom_settings=True,
+        **DEFAULT_IMPORT_SETTINGS,
+    )
+
+    texture_file = tmp_path / "model_with_embedded_textures" / "test_image.dds"
+    texture_contents_after_import = texture_file.read_bytes()
+
+    bpy.data.objects["model_with_embedded_textures"].select_set(True)
+
+    bpy.ops.sollumz.export_assets(
+        directory=str(tmp_path.absolute()),
+        direct_export=True,
+        use_custom_settings=True,
+        **DEFAULT_EXPORT_SETTINGS | {
+            "target_formats": {"CWXML"},
+            "target_versions": {"GEN8"},
+        },
+    )
+
+    texture_contents_after_export = texture_file.read_bytes()
+    assert texture_contents_after_export == texture_contents_after_import

dependencies.py
@@ -40,8 +40,8 @@ DEPENDENCIES = (
         "Core functionality for import/export of asset files.",
         True,
         "",
-        "1.1.0.dev0",
-        ("fad3897197c45a15f85d1a76e20eb2f7bf2eb4ea825e01d56aab63dbf4df109d",),
+        "1.1.0.dev1",
+        ("63313e98f550320650cbb7c9bbc296099cd7b57a47e065f43f83c690da339b77",),
     ),
     Dependency(
         "pymateria",

tests/shared.py
@@ -1,7 +1,13 @@
-import os
 import itertools
-from typing import Optional
+import os
 from pathlib import Path
+from typing import Optional
+
+import bpy
+import pytest
+from bpy.types import (
+    BlendData,
+)
 
 
 def get_env_path(name: str) -> Optional[Path]:
@@ -19,6 +25,7 @@ def get_env_path(name: str) -> Optional[Path]:
 SOLLUMZ_TEST_TMP_DIR = get_env_path("SOLLUMZ_TEST_TMP_DIR")
 SOLLUMZ_TEST_GAME_ASSETS_DIR = get_env_path("SOLLUMZ_TEST_GAME_ASSETS_DIR")
 SOLLUMZ_TEST_ASSETS_DIR = Path(__file__).parent.joinpath("assets/")
+SOLLUMZ_TEST_DATA_DIR = Path(__file__).parent.joinpath("data/")
 
 
 def is_tmp_dir_available() -> bool:
@@ -48,7 +55,31 @@ def glob_assets(ext: str) -> list[tuple[Path, str]]:
     return list(map(lambda p: (p, str(p)), assets))
 
 
-def asset_path(file_name: str) -> Path:
-    path = SOLLUMZ_TEST_ASSETS_DIR.joinpath(file_name)
+def asset_path(*path_segments: str) -> Path:
+    path = SOLLUMZ_TEST_ASSETS_DIR.joinpath(*path_segments)
     assert path.exists()
     return path
+
+
+def data_path(*path_segments: str) -> Path:
+    path = SOLLUMZ_TEST_DATA_DIR.joinpath(*path_segments)
+    assert path.exists()
+    return path
+
+
+def load_blend_data(file_name: str) -> BlendData:
+    bpy.ops.wm.open_mainfile(filepath=str(data_path(file_name)))
+    return bpy.data
+
+
+def _is_szio_native_available() -> bool:
+    import szio.gta5.native
+    return szio.gta5.native.IS_BACKEND_AVAILABLE
+
+
+requires_szio_native = pytest.mark.skipif(
+    not _is_szio_native_available(),
+    reason="test requires szio native backend to be available"
+)
+
+del _is_szio_native_available

ydd/yddexport_io.py
@@ -25,7 +25,7 @@ def export_ydd(dwd_obj: Object) -> ExportBundle:
         cld = cloth_char_export_dictionary(dwd_obj)
         dwd = create_drawable_dictionary_asset(dwd_obj, cld, out_embedded_textures=embedded_tex)
 
-    return export_context().make_bundle(dwd, ("", cld), files_to_copy=[t.source_filepath for t in embedded_tex])
+    return export_context().make_bundle(dwd, ("", cld), extra_files=[t.data for t in embedded_tex])
 
 
 def create_drawable_dictionary_asset(

commit c589d63a5b6c897e1c3a041b988ed0f405c66e75

fix: error when writing external textures to export directory
Regression introduced in 59a978518f0a0cfef7443c99642f4021e9c2b71b where
the check for dst_file existance was removed. `Path.samefile` throws an
error if either of the files doesn't exist.

iecontext.py
@@ -86,7 +86,11 @@ class ExportBundle:
                     res_directory.mkdir(exist_ok=True)
                     dst_file = res_directory / src_data.name
 
-                    if (src_file := getattr(src_data, "filepath", None)) and dst_file.samefile(src_file):
+                    if (
+                        (src_file := getattr(src_data, "filepath", None)) and
+                        dst_file.is_file() and
+                        dst_file.samefile(src_file)
+                    ):
                         # If src_data is a file and paths are the same, no need to copy (and would break otherwise)
                         continue
 

tests/test_import_export.py
@@ -17,6 +17,7 @@ from ..yft.yftimport import import_yft
 from .shared import (
     assert_logs_no_warnings_or_errors,
     asset_path,
+    data_path,
     glob_assets,
     is_tmp_dir_available,
     load_blend_data,
@@ -261,6 +262,27 @@ def test_export_model_with_packed_textures(tmp_path: Path):
     )
 
     expected_contents = data.images["test_image.dds"].packed_file.data
+    for expected_file in [
+        tmp_path / "gen8" / "test_model" / "test_image.dds",
+        tmp_path / "gen9" / "test_model" / "test_image.dds",
+    ]:
+        assert expected_file.is_file()
+        assert expected_file.read_bytes() == expected_contents
+
+
+@assert_logs_no_warnings_or_errors
+def test_export_model_with_external_textures(tmp_path: Path):
+    data = load_blend_data("model_with_external_textures.blend")
+
+    # .blend was saved with the object to export already selected
+    bpy.ops.sollumz.export_assets(
+        directory=str(tmp_path.absolute()),
+        direct_export=True,
+        use_custom_settings=True,
+        **DEFAULT_EXPORT_SETTINGS,
+    )
+
+    expected_contents = data_path("textures", "test_image.dds").read_bytes()
     for expected_file in [
         tmp_path / "gen8" / "test_model" / "test_image.dds",
         tmp_path / "gen9" / "test_model" / "test_image.dds",