1fffea8898aaf49207877e1ffcb7003e1828b074
Luan <123782375+LN-Development@users.noreply.github.com>
2026-07-15 14:00:01 -0300

    Revert "Small optimization that improves the O(N) of get_children_recursive"
    This reverts commit 7498131de4b335074196109adeeaeb549d1818f5.

--- tools/blenderhelper.py
+++ tools/blenderhelper.py
@@ -240,23 +240,19 @@ def get_armature_obj(armature):
 
 
 def get_children_recursive(obj) -> list[bpy.types.Object]:
-    if obj is None:
-        return []
-
-    children_by_parent: dict[bpy.types.Object, list[bpy.types.Object]] = {}
-    for o in bpy.data.objects:
-        parent = o.parent
-        if parent is not None:
-            children_by_parent.setdefault(parent, []).append(o)
-
     children = []
 
-    def _collect(parent):
-        for child in children_by_parent.get(parent, ()):
-            children.append(child)
-            _collect(child)
+    if obj is None:
+        return children
 
-    _collect(obj)
+    if len(obj.children) < 1:
+        return children
+
+    for child in obj.children:
+        children.append(child)
+        if len(child.children) > 0:
+            children.extend(get_children_recursive(child))
+
     return children
 
 
